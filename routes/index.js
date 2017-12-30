72890e417c8d4a3e31059c2d7d08bb44328df2da
DESKTOP-6EJ4SMA\Vansh <shahvansh123@gmail.com>
2017-12-29 21:21:02 -0800

    Sending Email Working correctly

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var config = require('./config.json');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-    var to = req.body.to;
-    var subject = req.body.subject;
-    var body = req.body.body;
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: config.email,
+            pass: config.password
+        }
+    });
 
-    console.log(to);
-    console.log(subject);
-    console.log(body);
+    var mailOptions = {
+        from: config.email,
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.body
+    };
 
-    res.send("Done");
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.send("--- Email Sent ---");
     res.end();
+
 });
 
 module.exports = router;
