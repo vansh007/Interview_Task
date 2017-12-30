233dd7573f507606473208253dce680f0c1e1f5b
DESKTOP-6EJ4SMA\Vansh <shahvansh123@gmail.com>
2017-12-29 23:14:33 -0800

    Added Email Validator

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,46 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var config = require('./config.json');
+var validator = require("email-validator");
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: config.email,
             pass: config.password
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
         }
     });
 
-    var mailOptions = {
-        from: config.email,
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.body
-    };
+    if (validator.validate(req.body.to)){
+        var mailOptions = {
+            from: config.email,
+            to: req.body.to,
+            subject: req.body.subject,
+            text: req.body.body
+        };
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        res.send("--- Email Sent ---");
+        res.end();
+    }else{
+        res.send("--- Invalid Email in Body ---");
+        res.end();
+    }
 
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-    res.send("--- Email Sent ---");
-    res.end();
+
 
 });
 
