e6340595a179be8e869da78147935591342a29e7
DESKTOP-6EJ4SMA\Vansh <shahvansh123@gmail.com>
2017-12-29 20:56:45 -0800

    Getting Body Data

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+    var to = req.body.to;
+    var subject = req.body.subject;
+    var body = req.body.body;
+
+    console.log(to);
+    console.log(subject);
+    console.log(body);
+
+    res.send("Done");
+    res.end();
 });
 
 module.exports = router;
